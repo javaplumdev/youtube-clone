f484b78c2c7d5b8c65b51f36174d0655591b01a9
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-09-10 21:27:27 +0800

    update

--- yt-clone/src/App.js
+++ yt-clone/src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import {
+	Navbar,
+	Feed,
+	VideoDetail,
+	ChannelDetails,
+	SearchFeed,
+} from './components';
+
 function App() {
 	return (
-		<div className="App">
-			<p>Hi</p>
-		</div>
+		<Router>
+			<Box sx={{ backgroundColor: '#000' }}>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Feed />} />
+					<Route path="/video/:id" element={<VideoDetail />} />
+					<Route path="/channel/:id" element={<ChannelDetails />} />
+					<Route path="/search/:searchTerm" element={<SearchFeed />} />
+				</Routes>
+			</Box>
+		</Router>
 	);
 }
 
