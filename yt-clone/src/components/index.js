56a4355bcec30a2b6042bf9e33424c65ff724ed4
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-09-10 23:23:22 +0800

    update

--- yt-clone/src/components/index.js
+++ yt-clone/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Feed } from './Feed';
 export { default as ChannelDetails } from './ChannelDetails';
 export { default as SearchFeed } from './SearchFeed';
 export { default as VideoDetail } from './VideoDetail';
+export { default as SearchBar } from './SearchBar';
